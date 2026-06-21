d3e0fe220ca9aea512784f8b1ba00228a12ff40e
Ritwik <ritwikbhowmik659@gmail.com>
2026-06-21 20:18:48 +0530

    feat:optimize the service code and use lombok

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -8,34 +8,15 @@ import jakarta.persistence.Id;
 import lombok.*;
 
 @Entity(name="categories")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Category {
-
-    public Category(){
-
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryId(Long categoryId){
-        this.categoryId=categoryId;
-    }
-    public void setCategoryName(String categoryName){
-        this.categoryName=categoryName;
-    }
 }
