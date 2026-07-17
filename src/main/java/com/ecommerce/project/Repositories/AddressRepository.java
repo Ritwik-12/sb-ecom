4c741505706dad0cac9629adde598ebd9e6eaf80
Ritwik <ritwikbhowmik659@gmail.com>
2026-07-17 08:28:15 +0530

    feat:Address dto and repository added

---
+++ src/main/java/com/ecommerce/project/Repositories/AddressRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.Repositories;
+
+import com.ecommerce.project.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address,Long> {
+}
